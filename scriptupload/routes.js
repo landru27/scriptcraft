0032be34743e7113703ea83208d446a553223a2a
Andrew Witt <landru729@gmail.com>
2017-12-28 11:49:04 -0500

    add logic for loading an existing script file

--- scriptupload/routes.js
+++ scriptupload/routes.js
@@ -2,19 +2,43 @@ const util = require('util');
 const fs = require('fs');
 
 const logfile = '/opt/spigot/scriptupload/upload.log';
+const scriptpath = '/opt/spigot/scriptcraft/plugins/scriptupload';
 
 module.exports = function(app){
 
   app.get('/scriptupload', function(req, res){
+    var scriptname = req.query.scriptname;
+    var scriptbody = 'non';
+
+    if (typeof scriptname == undefined) {
+      scriptname = 'filename';
+      scriptbody = 'JavaScript';
+    } else {
+      var scriptfile = scriptpath + '/' + scriptname;
+      scriptbody = fs.readFileSync(scriptfile, 'utf8');
+    }
+
     res.render('scriptupload', {
       msg: 'welcome!',
-      scriptname: 'filename',
-      scriptbody: 'JavaScript'
+      scriptname: scriptname,
+      scriptbody: scriptbody
+    });
+  });
+
+  app.get('/scriptlist', function(req, res){
+    var sl = [];
+
+    fs.readdirSync(scriptpath).forEach(file => {
+      sl.push(file);
+    });
+
+    res.render('scriptlist', {
+      msg: 'list of current scripts; click on a filename to load it for editing',
+      scriptlist: sl
     });
   });
 
   app.post('/scriptupload', function(req, res){
-    var scriptpath = '/opt/spigot/scriptcraft/plugins/scriptupload';
     var scriptname = req.body.scriptname;
 
     if (! scriptname.match(/\.js$/)) {
